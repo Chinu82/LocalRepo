72635969daa4aa506744086c37e02955d87f825a
Chinmaya <cinshu39@gmail.com>
2024-03-14 13:22:14 +0530

    change logic

--- cins.java
+++ cins.java
@@ -2,5 +2,6 @@ class Test{
     public static void main(String[] args) {
         System.out.println("Hello World ;");
         System.out.println("Lol Java");
+        System.out.println("Lol Git");
     }
 }
