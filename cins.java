c82ec6dee591ba1cbd86202016749f16b41976e6
Chinmaya <cinshu39@gmail.com>
2024-03-14 13:15:14 +0530

    Delete

--- cins.java
+++ cins.java
@@ -2,6 +2,5 @@ class Test{
     public static void main(String[] args) {
         System.out.println("Hello World ;");
         System.out.println("Lol Java");
-        System.out.println("Lol Git");
     }
 }
